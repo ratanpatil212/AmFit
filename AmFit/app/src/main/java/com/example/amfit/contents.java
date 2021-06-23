2c96e109fc2806abcaa7b6aee19c7f6ab8909d5c
ratanpatil212@gmail.com <ratanpatil212@gmail.com>
2021-06-23 23:55:43 +0530

    Connected everybody's pages to content page

--- AmFit/app/src/main/java/com/example/amfit/contents.java
+++ AmFit/app/src/main/java/com/example/amfit/contents.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class contents extends AppCompatActivity {
-    Button logout,b1;
+    Button logout,b1,b2,b3,b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +35,32 @@ public class contents extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),Specials.class));
+
+            }
+        });
+
+        b2 = findViewById(R.id.muscle_buildup);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),workoutmain2.class));
+
+            }
+        });
+
+        b3 = findViewById(R.id.mental_health);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity2.class));
+
+            }
+        });
+        b4 = findViewById(R.id.diet);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Diet.class));
 
             }
         });
